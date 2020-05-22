988777ba10122db3eb92c0d1e4650481d6904540
ថಘઅഞפּ ṛཥચ <42241901+arsium@users.noreply.github.com>
2020-05-22 09:55:43 +0200

    Add files via upload

---
+++ WebSocket_CLient/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Threading
+Public Class Form1
+    Public port As String = "8080"
+    Public ip As String = "127.0.0.1"
+    Public o As Socket
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        o = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        Dim MonEP As IPEndPoint = New IPEndPoint(IPAddress.Parse(ip), port)
+        o.Connect(MonEP)
+        Dim k As New Thread(Sub() ReadMSG(o))
+        k.Start()
+    End Sub
+    Private Sub ReadMSG(ByVal SocketReception As Socket)
+        While True
+            Try
+                Dim l(4096) As Byte
+                SocketReception.Receive(l)
+                MessageBox.Show(System.Text.Encoding.UTF8.GetString(l))
+            Catch ex As Exception
+            End Try
+        End While
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim oaz As Byte() = System.Text.Encoding.UTF8.GetBytes("Hello from Client !")
+        o.Send(oaz)
+    End Sub
+End Class
---
+++ WebSocket_Test/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+Imports System.Net.WebSockets
+Imports System.Text
+Imports System.Threading
+Public Class Form1
+    Public port As String = "8080"
+    Public ip As String = "127.0.0.1"
+    Public g As Socket
+    Public j As New List(Of Socket)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        g = New Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp)
+        g.Bind(New IPEndPoint(IPAddress.Parse(ip), port))
+        g.Listen(1)
+        Dim Context As TaskScheduler = TaskScheduler.FromCurrentSynchronizationContext()
+        Task.Factory.StartNew(Sub() AcceptAndRead(Context), CancellationToken.None, TaskCreationOptions.LongRunning)
+    End Sub
+    Public Sub AcceptAndRead(ByVal Context As TaskScheduler)
+        Try
+            While (True)
+                Dim p As Socket = g.Accept
+                j.Add(p)
+                ListView1.Items.Add(p.LocalEndPoint.ToString)
+                Task.Run(Sub() RD(p, Context, p.LocalEndPoint.ToString))
+            End While
+        Catch ex As Exception
+            Exit Sub
+        End Try
+    End Sub
+    ''  Public iop As StringBuilder
+    Private Sub RD(ByVal stream As Socket, ByVal context As TaskScheduler, ByVal id As String)
+        ''
+        ''   iop = New StringBuilder
+        While True
+            Dim Buffer(4096) As Byte
+            stream.Receive(Buffer)
+            Try
+                Dim Message As String = Encoding.UTF8.GetString(Buffer)
+                MessageBox.Show(Message)
+            Catch ex As Exception
+            End Try
+        End While
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For Each k As Socket In j
+            Dim o As Byte() = System.Text.Encoding.UTF8.GetBytes("Hello from server !")
+            k.Send(o)
+        Next
+    End Sub
+End Class
